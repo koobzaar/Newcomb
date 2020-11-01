9e891617e21e66b838dac819c60a4d9aa84a3d48
Kobzar <bruno.trigueiro@outlook.com>
2020-11-01 10:25:07 -0300

    Pequenas alterações visuais e formatação de código
    Alteramos algumas características do front-end na página inicial, como a adição do ditado chinês para impacto ao texto da pesquisa.
    Também formatamos o código fonte de alguns scripts e comentamos para melhor entendimento de terceiros.

--- script/graphGenerator.js
+++ script/graphGenerator.js
@@ -1,42 +1,52 @@
-function definirCorCoeficiente(x){
-    if(x<18)
-    return ` <span style='float:right' class='bom'>`+Math.floor(x)+`</span>`
-    if(x>=18&&x<25)
-    return `<span style='float:right' class='medio'>`+Math.floor(x)+`</span>`
-    if(x>25)
-    return `<span style='float:right' class='ruim'>`+Math.floor(x)+`</span>`
+function definirCorCoeficiente(x) {
+    if (x < 18)
+        return ` <span style='float:right' class='bom'>` + Math.floor(x) + `</span>`
+    if (x >= 18 && x < 25)
+        return `<span style='float:right' class='medio'>` + Math.floor(x) + `</span>`
+    if (x > 25)
+        return `<span style='float:right' class='ruim'>` + Math.floor(x) + `</span>`
 }
-function gerarGrafico(idDiv,benfordData, countryColor,nomePais, coeficiente){
-    if(nomePais==undefined)
-    nomePais=' ';
+i=0;
+/** 
+ *  @string idDiv: código ISO 3166 para as bandeiras dos países
+ *  @intArray benfordData: um array de tamanho 9 que armazena os primeiros digitos das mortes de cada país no formato [(quantidadeNumeros1),(quantidadeNumeros2)...(quantidadeNumeros9)]
+ *  @string countryColor: cor RGB dominante da bandeira do país em questão
+ *  @string nomePais: nome do país
+ *  @int coeficiente: coeficiente de similaridade
+ */
+function gerarGrafico(idDiv, benfordData, countryColor, nomePais, coeficiente) {i++
+    if (nomePais == undefined)
+        nomePais = ' ';
+    
     document.querySelector('.benford').insertAdjacentHTML(
         'afterbegin',
-        `<div class='grafico'>
-        <img class='bandeiras' src="https://www.countryflags.io/`+idDiv+`/flat/32.png">&nbsp;`+nomePais+``+definirCorCoeficiente(coeficiente)+`
-        <canvas id="`+idDiv+`" width="350" height="300"></canvas>
-        </div>`      
-      )
+        `<div class='grafico' id="`+nomePais+`">
+        <img class='bandeiras' src="https://www.countryflags.io/` + idDiv + `/flat/32.png">&nbsp;` + nomePais + `` + definirCorCoeficiente(coeficiente) + `
+        <canvas id="` + idDiv + `" width="350" height="300"></canvas>
+        </div>`
+    )
     var ctx = document.getElementById(idDiv).getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9'],
             datasets: [{
-                label: 'Aplicação da Lei de Benford (%)',
-                data: benfordData,
-                backgroundColor: [
-                    'rgba('+countryColor+', 0.2)',
-                ],
-                borderColor: [
-                    'rgba('+countryColor+', 1)',
-                ],
-                borderWidth: 1
-            },
-        {
-            data: [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6],
-            label: 'Proporção de Benford (%)',
-            
-        }]
+                    label: 'Aplicação da Lei de Benford (%)',
+                    data: benfordData,
+                    backgroundColor: [
+                        'rgba(' + countryColor + ', 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(' + countryColor + ', 1)',
+                    ],
+                    borderWidth: 1
+                },
+                {
+                    data: [30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6],
+                    label: 'Proporção de Benford (%)',
+
+                }
+            ]
         },
         options: {
             scales: {
--- script/mainScript.js
+++ script/mainScript.js
@@ -1,73 +1,105 @@
 function analisar() {
   const analisePrincipal = new analisador()
   analisePrincipal._dePaises();
-    $('.CountMortes').each(function () {
-      var $this = $(this);
-      jQuery({
-        Counter: 0
-      }).animate({
-        Counter: analisePrincipal.mortesTotais
-      }, {
-        duration: 1000,
-        easing: 'swing',
-        step: function () {
-          $this.text(Math.ceil(this.Counter).toLocaleString('pt') + ' mil óbitos');
-        }
+  let contadorConfig = {
+    "duracao": 4000
+  }
+ 
+var element = document.getElementById('descricaoGraficos');
+var elementHeight = element.clientHeight;
+
+document.addEventListener('scroll', animate);
+function inView() {
+  var windowHeight = window.innerHeight;
+  var scrollY = window.scrollY || window.pageYOffset;
+  
+  var scrollPosition = scrollY + windowHeight;
+  var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
+  
+  if (scrollPosition > elementPosition) {
+    return true;
+  }
+  
+  return false;
+}
+var animateOnce=false
+function animate() {
+  if (inView()) {
+    if(animateOnce===false){
+
+      animateOnce=true
+      $('.CountMortes').each(function () {
+        var $this = $(this);
+        jQuery({
+          Counter: 0
+        }).animate({
+          Counter: analisePrincipal.mortesTotais
+        }, {
+          duration: contadorConfig.duracao,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.ceil(this.Counter).toLocaleString('pt') + ' mil vidas perdidas');
+          }
+        });
       });
-    });
-    $('.CountDias').each(function () {
-      var $this = $(this);
-      jQuery({
-        Counter: 0
-      }).animate({
-        Counter: analisePrincipal.diasAcumuladosAnalisados
-      }, {
-        duration: 1000,
-        easing: 'swing',
-        step: function () {
-          $this.text(Math.ceil(this.Counter).toLocaleString('pt') + ' dias de dados acumulativos');
-        }
+      $('.CountDias').each(function () {
+        var $this = $(this);
+        jQuery({
+          Counter: 0
+        }).animate({
+          Counter: analisePrincipal.diasAcumuladosAnalisados
+        }, {
+          duration: contadorConfig.duracao,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.ceil(this.Counter).toLocaleString('pt') + ' dias de dados acumulativos');
+          }
+        });
       });
-    });
-    $('.CountAnos').each(function () {
-      var $this = $(this);
-      jQuery({
-        Counter: 0
-      }).animate({
-        Counter: analisePrincipal.diasAcumuladosAnalisados / 365
-      }, {
-        duration: 1000,
-        easing: 'swing',
-        step: function () {
-          $this.text(Math.ceil(this.Counter).toLocaleString('pt') + ' anos de dados acumulativos de pandemia');
-        }
+      $('.CountAnos').each(function () {
+        var $this = $(this);
+        jQuery({
+          Counter: 0
+        }).animate({
+          Counter: analisePrincipal.diasAcumuladosAnalisados / 365
+        }, {
+          duration: contadorConfig.duracao,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.ceil(this.Counter).toLocaleString('pt') + ' anos de dados acumulativos de pandemia');
+          }
+        });
       });
-    });
-  $('.Contador').each(function () {
-    var $this = $(this);
-    jQuery({
-      Counter: 0
-    }).animate({
-      Counter: $(this).text()
-    }, {
-      duration: 1000,
-      easing: 'swing',
-      step: function () {
-        $this.text(Math.ceil(this.Counter).toLocaleString('pt'));
-      }
-    });
-  });$('.ContadorPaises').each(function () {
-    var $this = $(this);
-    jQuery({
-      Counter: 0
-    }).animate({
-      Counter: analisePrincipal.paisesAnalisados.length
-    }, {
-      duration: 1000,
-      easing: 'swing',
-      step: function () {
-        $this.text(Math.ceil(this.Counter).toLocaleString('pt'));
-      }
-    });
-  });
+      $('.Contador').each(function () {
+        var $this = $(this);
+        jQuery({
+          Counter: 0
+        }).animate({
+          Counter: 1187014
+        }, {
+          duration: contadorConfig.duracao,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.ceil(this.Counter).toLocaleString('pt'));
+          }
+        });
+      });
+      $('.ContadorPaises').each(function () {
+        var $this = $(this);
+        jQuery({
+          Counter: 0
+        }).animate({
+          Counter: analisePrincipal.paisesAnalisados.length
+        }, {
+          duration: contadorConfig.duracao,
+          easing: 'swing',
+          step: function () {
+            $this.text(Math.ceil(this.Counter).toLocaleString('pt'));
+          }
+        });
+      });
+  }} 
+}
+
+     
 }
